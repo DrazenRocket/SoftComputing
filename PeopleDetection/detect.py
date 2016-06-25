3a0ca6744a8ddf7b5bb9e51128918d914d5f9bf1
DrazenRocket <drazen.rocket@hotmail.com>
2016-06-25 16:07:41 +0200

    Grubo prebojavanje i označavanje ljudi je odrađeno

--- PeopleDetection/detect.py
+++ PeopleDetection/detect.py
@@ -8,6 +8,32 @@ video_path = "test_video/TownCentreXVID.avi"
 frame_width = 450
 frame_height = 250
 
+def mark_people(gray, original):
+    count_people = 0;
+    ret, threshold = cv2.threshold(gray, 5, 255, cv2.THRESH_BINARY)
+    cv2.imshow('threshold', threshold)
+
+    # threshold = cv2.dilate(threshold, np.ones((5, 5)))
+    # threshold = cv2.erode(threshold, np.ones((5, 5)))
+    threshold = cv2.erode(threshold, np.ones((3, 3)))
+    # threshold = cv2.dilate(threshold, np.ones((3, 3)))
+
+    contours_img, contours, hierarchy = cv2.findContours(threshold.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    cv2.imshow('contours', contours_img)
+
+    for contour in contours:
+        x, y, w, h = cv2.boundingRect(contour)
+        if (w>20 and h>20):
+            cv2.rectangle(original, (x,y), (x+w, y+h), (0,0,255),2)
+            count_people = count_people+1
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    text = 'Count of people: '+str(count_people)
+    cv2.putText(original, text, (10,10), font, 0.5, (255,0,0), 2,cv2.LINE_AA)
+    cv2.imshow('Obelezeni ljudi', original)
+
+
+
 # Inicijalizacija rada sa videom (otvaranje videa)
 cap = cv2.VideoCapture(video_path)
 
@@ -22,12 +48,12 @@ prvs = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
 hsv = np.zeros_like(frame1)                 # np.zeros_like(frame1) vraca niz istih dimenzija kao frame1 ali sa nulama
 hsv[..., 1] = 255                           # green
 
-while (True):
+while (cap.isOpened()):
     # Citanje drugog tj. treceg frejma i promena njegove dimenzije
     ret, frame2 = cap.read()
     ret, frame2 = cap.read()
     frame2 = cv2.resize(frame2, (frame_width, frame_height))
-    fr = np.copy(frame2)                                        # Originalni sledeci frejm (zbog prikaza)
+    frame2_org = np.copy(frame2)                                        # Originalni sledeci frejm (zbog prikaza)
 
     # Pretvaranje drugog tj. treceg frejma u sivi
     next = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
@@ -44,9 +70,12 @@ while (True):
 
     # Konvertovanje hsv u boju
     rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+    gray = cv2.cvtColor(rgb, cv2.COLOR_RGB2GRAY)
 
-    cv2.imshow('Original', fr)
-    cv2.imshow('Detektovano', rgb)
+    cv2.imshow('Original', frame2_org)
+    cv2.imshow('Detektovano rgb', rgb)
+    cv2.imshow('Detektovano sivo', gray)
+    mark_people(gray.copy(), frame2_org.copy())
 
     k = cv2.waitKey(30) & 0xff
     if k == ord('q'):
